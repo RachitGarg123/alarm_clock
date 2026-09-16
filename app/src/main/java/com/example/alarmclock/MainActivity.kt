eee5764de2a86e4658a7d014a645dfe30c92bbdb
Rachit Garg <rachitgarg@Rachits-MacBook-Air.local>
2026-09-16 19:44:40 +0530

    unused imports removed

--- app/src/main/java/com/example/alarmclock/MainActivity.kt
+++ app/src/main/java/com/example/alarmclock/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
--- app/src/main/java/com/example/alarmclock/di/DBProvider.kt
+++ app/src/main/java/com/example/alarmclock/di/DBProvider.kt
@@ -2,7 +2,6 @@ package com.example.alarmclock.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.alarmclock.alarmset.data.db.Alarm
 import com.example.alarmclock.alarmset.data.db.AlarmDao
 import com.example.alarmclock.alarmset.data.db.AlarmDatabase
 import dagger.Module
