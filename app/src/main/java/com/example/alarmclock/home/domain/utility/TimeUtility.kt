c1b8801435cd5a9f7568ae5c50626d7929ca5001
Rachit Garg <rachitgarg@Rachits-MacBook-Air.local>
2026-09-19 00:01:43 +0530

    minor UI bug resolved

--- app/src/main/java/com/example/alarmclock/home/domain/utility/TimeUtility.kt
+++ app/src/main/java/com/example/alarmclock/home/domain/utility/TimeUtility.kt
@@ -4,7 +4,7 @@ package com.example.alarmclock.home.domain.utility
 fun getTwelveHourFormat(alarmTime: String): String {
     val alarmHour = alarmTime.take(2).toIntOrNull() ?: return "--:--"
     if(alarmHour == 0) {
-        return "12 + ${alarmTime.substring(startIndex = 2, endIndex = alarmTime.length)}"
+        return "12${alarmTime.substring(startIndex = 2, endIndex = alarmTime.length)}"
     }
     return if(alarmHour < 13) { // 08:00
         alarmTime.substring(startIndex = 1, endIndex = alarmTime.length)
